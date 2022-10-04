dc0dc63cd6ca2da25e3931d3b03818e4a67dab3d
Mohamed Kabbou <103792068+LifeTakerSimo@users.noreply.github.com>
2022-10-04 17:08:04 +0200

    Initial commit

---
+++ shell1.c
@@ -0,0 +1,151 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+
+#include <sys/types.h>
+#include <signal.h>
+// execute the commande 
+
+static char	**split(char *raw_cmd, char *limit)
+{
+	char	*ptr = NULL;
+	char	**cmd = NULL;
+	size_t	idx = 0;
+
+	// split sur les espaces
+	ptr = strtok(raw_cmd, limit);
+
+	while (ptr) {
+		cmd = (char **)realloc(cmd, ((idx + 1) * sizeof(char *)));
+		cmd[idx] = strdup(ptr);
+		ptr = strtok(NULL, limit);
+		++idx;
+	}
+	// On alloue un element qu'on met a NULL a la fin du tableau
+	cmd = (char **)realloc(cmd, ((idx + 1) * sizeof(char *)));
+	cmd[idx] = NULL;
+	return (cmd);
+}
+
+static void	free_array(char **array)
+{
+	for (int i = 0; array[i]; i++) {
+		free(array[i]);
+		array[i] = NULL;
+	}
+	free(array);
+	array = NULL;
+}
+
+static void	exec_cmd(char **cmd)
+{
+	pid_t	pid = 0;
+	int		status = 0;
+
+	// On fork
+	pid = fork();
+	if (pid == -1)
+		perror("fork");
+	// Si le fork a reussit, le processus pere attend l'enfant (process fork)
+	else if (pid > 0) {
+		// On block le processus parent jusqu'a ce que l'enfant termine puis
+		// on kill le processus enfant
+		waitpid(pid, &status, 0);
+		kill(pid, SIGTERM);
+	} else {
+		// Le processus enfant execute la commande ou exit si execve echoue
+		if (execve(cmd[0], cmd, NULL) == -1)
+			perror("shell");
+		exit(EXIT_FAILURE);
+	}
+}
+
+
+
+static void	get_absolute_path(char **cmd)
+{
+	char	*path = strdup(getenv("PATH"));
+	char	*bin = NULL;
+	char	**path_split = NULL;
+
+	if (path == NULL) // si le path est null, on cree un path
+		path = strdup("/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/sbin");
+
+	// si cmd n'est pas le chemin absolue, on cherche le chemin absolue du
+	// binaire grace a la variable d'environment PATH
+	if (cmd[0][0] != '/' && strncmp(cmd[0], "./", 2) != 0) {
+
+		// On split le path pour verifier ou ce trouve le binaire
+		path_split = split(path, ":");
+		free(path);
+		path = NULL;
+
+		// On boucle sur chaque dossier du path pour trouver l'emplacement du binaire
+		for (int i = 0; path_split[i]; i++) {
+			// alloc len du path + '/' + len du binaire + 1 pour le '\0'
+			bin = (char *)calloc(sizeof(char), (strlen(path_split[i]) + 1 + strlen(cmd[0]) + 1));
+			if (bin == NULL)
+				break ;
+
+			// On concat le path , le '/' et le nom du binaire
+			strcat(bin, path_split[i]);
+			strcat(bin, "/");
+			strcat(bin, cmd[0]);
+
+			// On verfie l'existence du fichier et on quitte la boucle si access
+			// renvoi 0
+			if (access(bin, F_OK) == 0)
+				break ;
+
+			// Nous sommes des gens propre :D
+			free(bin);
+			bin = NULL;
+		}
+		free_array(path_split);
+
+		// On remplace le binaire par le path absolue ou NULL si le binaire
+		// n'existe pas
+		free(cmd[0]);
+		cmd[0] = bin;
+	} else {
+		free(path);
+		path = NULL;
+	}
+}
+
+
+int	main()
+{
+	char	*buffer = NULL;
+	size_t	buf_size = 2048;
+
+	// alloc buffer qui stockera la commande entree par l'user
+	buffer = (char *)calloc(sizeof(char), buf_size);
+	if (buffer == NULL) {
+		perror("Malloc failure");
+		return (EXIT_FAILURE);
+	}
+
+	// ecriture d'un prompt
+	write(1, "$> ", 3);
+
+	// lecture de STDIN en boucle
+	while (getline(&buffer, &buf_size, stdin) > 0) {
+		char **cmd = split(buffer, " \n\t");
+		get_absolute_path(cmd);
+
+		if (cmd[0] == NULL)
+			printf("Command not found\n");
+		else
+			exec_cmd(cmd);
+
+		write(1, "$> ", 3);
+		free_array(cmd);
+
+	}
+
+	printf("Bye \n");
+	free(buffer);
+	}
